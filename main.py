a2dbd49a2af2a562b35b1f10029d19aaa54f8213
ivanamol <ivaryd@post.cz>
2025-10-05 20:32:56 +0200

    Přidána fce generator_cisel_hra

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# """
+# projekt2.py: druhý projekt do Engeto Online kurzu Tester s Pythonem
+
+# author: Ivana Molnárová
+# email: ivaryd@post.cz
+# """
+
+from random import randint
+
 uvodni_text = """Hi there!
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
@@ -15,3 +24,14 @@ print(f"""{upraveny_text[0]}
 
 cislo = input(f"""{upraveny_text[3]:} """)
 print(f"""{"-" * (max(delky_vet))}""")
+
+def generator_cisel_hra(pocet_cisel):
+    vybrana_cisla = set()
+    while len(vybrana_cisla) < pocet_cisel:
+        cislo = randint(0, 9)
+        vybrana_cisla.add(cislo)
+        cisla_ke_hre = str(vybrana_cisla).replace("{","").replace(".","").replace("}","").replace(",","").replace(" ","")
+        if cisla_ke_hre[0] == "0":
+            vybrana_cisla.remove(0)
+    return cisla_ke_hre
+    
