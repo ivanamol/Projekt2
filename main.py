cb22c92207f9f4c75043f00ad9023ce60b41b9aa
ivanamol <ivaryd@post.cz>
2025-10-12 14:55:01 +0200

    Upravena fce uprav_uvodni_text, přidána nová fce pocet_znaku_nejdelsi_vety, vytvořena základní struktura kódu pro funkční hru, while smyčka

--- main.py
+++ main.py
@@ -7,17 +7,20 @@
 
 from random import randint
 
-def uprav_uvodni_text(text: str):
+def pocet_znaku_nejdelsi_vety(text: str):
     upraveny_text = text.split("\n")
     delky_vet = [len(veta) for veta in upraveny_text]
-    print(f"""{upraveny_text[0]}
-{"-" * (max(delky_vet))}
+    return max(delky_vet)
+
+def uprav_uvodni_text(text: str):
+    upraveny_text = text.split("\n")
+    return print(f"""{upraveny_text[0]}
+{"-" * pocet_znaku_nejdelsi_vety(text)}
 {upraveny_text[1]}
 {upraveny_text[2]}
-{"-" * (max(delky_vet))}""")
-    hrac_hada = input(f"""{upraveny_text[3]:} """)
-    return hrac_hada
-
+{"-" * pocet_znaku_nejdelsi_vety(text)}
+{upraveny_text[3]:}
+{"-" * pocet_znaku_nejdelsi_vety(text)}""")
 
 def generator_cisel_hra(pocet_cisel: int):
     vybrana_cisla = set()
@@ -29,27 +32,24 @@ def generator_cisel_hra(pocet_cisel: int):
             vybrana_cisla.remove(0)
     return cisla_ke_hre
 
-
 def over_vstup_hrace(hracovo_hadani: str, pocet_cisel: int, ):
     if not hracovo_hadani.isdigit():
         return print("The input other than a number.")
     elif len(hracovo_hadani) != pocet_cisel:
         return print(f"The input does not contain exactly {pocet_cisel} numbers.")
-    elif not hracovo_hadani.isdigit():
-        return print("The input other than a number")
     elif hracovo_hadani[0] == "0":
         return print("The number must not start with a zero.")
     elif len(hracovo_hadani) != len(set(hracovo_hadani)):
-        return print("The number must not contain duplicates")
+        return print("The number must not contain duplicates.")
+    else:
+         return "OK"
     
-
 def jednotne_nebo_mnozne(number: int, objekt: str):
     if number == 1:
         return objekt
     else:
         return objekt + "s"
     
-
 def pocet_bull(hracovo_hadani: str, vygenerovane_cislo: str):
     bull = 0
     if (hracovo_hadani[0] == vygenerovane_cislo[0]):
@@ -63,7 +63,6 @@ def pocet_bull(hracovo_hadani: str, vygenerovane_cislo: str):
     pocet = jednotne_nebo_mnozne(bull, "bull")
     return print(f"{bull} {pocet}")
 
-
 def pocet_cow(hracovo_hadani: str, vygenerovane_cislo: str):
     cow = 0
     if (hracovo_hadani[0] != vygenerovane_cislo[0]) and (hracovo_hadani[0] in vygenerovane_cislo):
@@ -76,10 +75,6 @@ def pocet_cow(hracovo_hadani: str, vygenerovane_cislo: str):
             cow = cow + 1
     pocet = jednotne_nebo_mnozne(cow, "cow")
     return print(f"{cow} {pocet}")
-    
-
-# pocet_bull(str(1234), str(1324))
-# pocet_cow(str(1234), str(6935))
 
 uvodni_text = """Hi there!
 I've generated a random 4 digit number for you.
@@ -89,50 +84,20 @@ Enter a number:
 
 vygenerovane_cislo = generator_cisel_hra(4)
 print(vygenerovane_cislo)
-
-hrac_hada = uprav_uvodni_text(uvodni_text)
-print(hrac_hada)
-
+uprav_uvodni_text(uvodni_text)
+hrac_hada = input(">>> ")
 over_vstup_hrace(hrac_hada, 4)
-print(f"{"-" * 47}")
-
 pokus = 1
-
-
-
-# while hrac_hada != vygenerovane_cislo:
-#     cow = 0
-#     if (hrac_hada[0] != vygenerovane_cislo[0]) and (hrac_hada[0] in vygenerovane_cislo):
-#         cow = cow + 1
-#     if (hrac_hada[1] != vygenerovane_cislo[1]) and (hrac_hada[1] in vygenerovane_cislo):
-#         cow = cow + 1
-#     if (hrac_hada[2] != vygenerovane_cislo[2]) and (hrac_hada[2] in vygenerovane_cislo):
-#         cow = cow + 1
-#     if (hrac_hada[3] != vygenerovane_cislo[3]) and (hrac_hada[3] in vygenerovane_cislo):
-#         cow = cow + 1
-#     pocet = jednotne_nebo_mnozne(cow, "cow")
-#     print(f"{cow} {pocet}")
-#     hrac_hada = input("Enter a number: ")
-#     over_vstup_hrace(hrac_hada, 4)
-# else:
-#     print(f"Correct, you've guessed the right number in {pokus} guesses!")
-
-
-
-# while hrac_hada != vygenerovane_cislo:
-#     bull = 0
-#     if (hrac_hada[0] == vygenerovane_cislo[0]):
-#         bull = bull + 1
-#     if (hrac_hada[1] == vygenerovane_cislo[1]):
-#         bull = bull + 1
-#     if (hrac_hada[2] == vygenerovane_cislo[2]):
-#         bull = bull + 1
-#     if (hrac_hada[3] == vygenerovane_cislo[3]):
-#         bull = bull + 1
-#     pocet = jednotne_nebo_mnozne(bull, "bull")
-#     print(f"{bull} {pocet}")
-#     hrac_hada = input("Enter a number: ")
-#     if over_vstup_hrace(hrac_hada, 4) != "OK":
-#             hrac_hada = input("Enter a number: ")
-# else:
-#     print(f"Correct, you've guessed the right number in {pokus} guesses!")
+while hrac_hada != vygenerovane_cislo:
+     pokus = pokus + 1
+     pocet_bull(hrac_hada, vygenerovane_cislo)
+     pocet_cow(hrac_hada, vygenerovane_cislo)
+     print(f"{"-" * pocet_znaku_nejdelsi_vety(uvodni_text)}")
+     hrac_hada = input(">>> ")
+     while over_vstup_hrace(hrac_hada, 4) != "OK":
+        print(f"{"-" * pocet_znaku_nejdelsi_vety(uvodni_text)}")
+        hrac_hada = input(">>> ")
+else:
+    print("Correct, you've guessed the right number\nin " + str(pokus) + " guesses!")
+    print(f"{"-" * pocet_znaku_nejdelsi_vety(uvodni_text)}")
+    print("That's amazing!")
